96b757f23558f1016369f2be078151ec08c4299f
Phillip Højbjerg <philliphoejbjerg@gmail.com>
2023-04-21 09:19:06 +0200

    pruning evaluation script added

--- src/models/test.py
+++ src/models/test.py
@@ -51,3 +51,62 @@ def run_inference(loader, model, device):
         torch.hstack(all_targets).cpu().numpy(), 
         torch.hstack(equals).cpu().numpy()
     )
+
+
+def run_inference_with_computational_metrics(loader, model, device):
+    latent_representations, predictions, equals, all_targets, probs = [], [], [], [], []
+    memory_usage_data = []
+
+    os.remove("emissions.csv")
+    tracker = EmissionsTracker()
+    
+    with torch.no_grad():
+        for batch in tqdm(iter(loader)):
+        
+            x       = batch['data'].to(device)
+            targets = batch['label'].to(device).to(torch.long)
+
+            tracker.start()
+            outputs = model(x)
+            memory_usage_data.append(memory_usage()[-1])
+            tracker.stop()
+
+            # Store latent representations
+            latent_representations.append(outputs['z'].cpu())
+
+            # Store predictions
+            preds = outputs['t_logits'].argmax(axis=1)
+            predictions.append(preds)
+            equals.append(torch.tensor(preds.cpu().numpy() == targets.cpu().numpy()))
+
+            # Store probabilities
+            probs.append(outputs['t_logits'].softmax(dim=1)) 
+
+            # Store targets
+            all_targets.append(targets)
+
+        computational_metrics = pd.read_csv('emissions.csv')[[
+            'timestamp', 'project_name', 'duration', 
+            'emissions', 'emissions_rate', 
+            'cpu_power', 'gpu_power', 'ram_power', 
+            'cpu_energy', 'gpu_energy', 'ram_energy', 'energy_consumed', 
+            'os', 'python_version', 
+            'cpu_count', 'cpu_model', 'gpu_count', 'gpu_model', 
+            'ram_total_size'
+        ]]
+
+        # Duration is continuously accumulated
+        computational_metrics['duration'] = [computational_metrics['duration'][0]]+[computational_metrics['duration'][i+1] 
+                                            - computational_metrics['duration'][i] for i in range(len(computational_metrics['duration']) - 1)]
+        computational_metrics['memory_usage'] = memory_usage_data
+
+    # Stack latent representations to one frame
+    return (
+        torch.vstack(latent_representations).cpu().numpy(), 
+        torch.hstack(predictions).cpu().numpy(), 
+        torch.vstack(probs).cpu().numpy(), 
+        torch.hstack(all_targets).cpu().numpy(), 
+        torch.hstack(equals).cpu().numpy(),
+        computational_metrics
+
+    )
